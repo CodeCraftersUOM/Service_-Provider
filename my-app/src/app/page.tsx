f8c8eae5915df461d5304b24369b85db4bb3752c
Dilshanrad22 <dulanjanadkdh0@gmail.com>
2025-04-02 00:45:07 +0530

    service_provider_registaation

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,23 @@
 import Login from "./components/login";
 import Middle from "./components/Middle";
 
-
 export default function Home() {
   return (
-    <main>
-      
+    <main
+      style={{
+        backgroundImage: "url('/propic.png')", // Path to image in public folder
+        backgroundSize: "cover", // Ensures it covers the full page
+        backgroundPosition: "center", // Centers the image
+        height: "100vh", // Full height of viewport
+        width: "100vw", // Full width of viewport
+        display: "flex", // Allows content positioning
+        flexDirection: "column", // Stacks children vertically
+        alignItems: "center", // Centers horizontally
+        justifyContent: "center", // Centers vertically
+      }}
+    >
       <Middle />
-      <Login/>
+      <Login />
     </main>
   );
 }
