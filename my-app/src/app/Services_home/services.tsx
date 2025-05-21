d05c5895252417f42b853841c40f6f00d6235559
Dilshanrad22 <dulanjanadkdh0@gmail.com>
2025-05-21 09:59:57 +0530

    update new page changes

--- my-app/src/app/Services_home/services.tsx
+++ my-app/src/app/Services_home/services.tsx
@@ -1,35 +1,56 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import styles from "./services.module.css";
 
+const serviceIcons: string[] = [
+  "/guide.png",
+  "/health.png",
+  "/accomadation.png",
+  "/resturant.png",
+  "/auto.png",
+  "/taxi.png",
+  "/communucation.png",
+  "/house.png",
+  "/other.png"
+];
+
+const serviceNames: string[] = [
+  "Guide",
+  "Health",
+  "Accommodation",
+  "Restaurant",
+  "Vehicle Service center",
+  "Taxi",
+  "Communication",
+  "House Keeping",
+  "Other Services"
+];
+
+const serviceLinks: string[] = [
+  "/guide",
+  "/health",
+  "/accommodation",
+  "/restaurant",
+  "/vehicle-service",
+  "/taxi",
+  "/communication",
+  "/house-keeping",
+  "/other-services"
+];
+
 const ServiceSelector: React.FC = () => {
-  const serviceIcons: string[] = [
-    "/guide.png",
-    "/health.png",
-    "/accomadation.png",
-    "/resturant.png",
-    "/public_transport.png",
-    "/other.png"
-  ];
+  const router = useRouter();
 
-  const serviceNames: string[] = [
-    "Guide",
-    "Health",
-    "Accommodation",
-    "Restaurant",
-    "Public Transport",
-    "Other"
-  ];
-
-  const serviceLinks: string[] = [
-    "/guide",
-    "/health",
-    "/accommodation",
-    "/restaurant",
-    "/public-transport",
-    "/otherservices"
-  ];
+  const handleLogout = () => {
+    // Implement your actual logout logic here
+    localStorage.removeItem("authToken");
+    sessionStorage.removeItem("userSession");
+    router.push("/login");
+  };
 
   return (
     <div className={styles.container}>
@@ -37,62 +58,47 @@ const ServiceSelector: React.FC = () => {
       <div className={styles.sidebar}>
         <div className={styles.user}>● User Name</div>
         <ul className={styles.menu}>
-          <li>My profile</li>
+          <li>My Profile</li>
           <li>Notification</li>
-          <li>Message</li>
-          <li>Feedback</li>
           <li>Help</li>
           <li>Orders</li>
           <li>Booking</li>
           <li>Payments</li>
           <li>Setting</li>
           <li>Updates</li>
-          <li>Other service</li>
-          <li>Log out</li>
+          <li>Other Services</li>
+          <li 
+            onClick={handleLogout}
+            className={styles.logoutItem}
+          >
+            Log Out
+          </li>
         </ul>
       </div>
 
-      {/* Content Area */}
-      <div className={styles.contentArea}>
-        <div className={styles.wrapper}>
-          <h2>
-            3 SIMPLE STEPS TO START
-            <br />
-            SERVICE PROVIDE
-            <br />
-            ON TRAVELWISH
-          </h2>
-          <div className={styles.steps}>
-            <div className={styles.stepBox}></div>
-            <div className={styles.stepBox}></div>
-            <div className={styles.stepBox}></div>
-          </div>
+      {/* Main Service Selection Grid */}
+      <div className={styles.mainContent}>
+        <h1>What service do you hope to provide?</h1>
+        <div className={styles.grid}>
+          {serviceIcons.map((icon, index) => (
+            <div className={styles.serviceCard} key={index}>
+              <div style={{ textAlign: "center" }}>
+                <p className={styles.serviceLabel}>{serviceNames[index]}</p>
+                <Link href={serviceLinks[index]}>
+                  <Image
+                    src={icon}
+                    alt={serviceNames[index]}
+                    width={150}
+                    height={150}
+                    className={styles.serviceImage}
+                  />
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
-
-        <div className={styles.mainContent}>
-          <h1>What service do you hope to provide?</h1>
-          <div className={styles.grid}>
-            {serviceIcons.map((icon, index) => (
-              <Link href={serviceLinks[index]} key={index}>
-                <div className={styles.serviceCard}>
-                  <div style={{ textAlign: "center" }}>
-                    <p className={styles.serviceLabel}>{serviceNames[index]}</p>
-                    <Image
-                      src={icon}
-                      alt={serviceNames[index]}
-                      width={150}
-                      height={150}
-                      className={styles.serviceImage}
-                    />
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div>
-
         <div className={styles.helpText}>
-          Need a <a href="#">help?</a>
+          Need <a href="#">help?</a>
         </div>
       </div>
     </div>
