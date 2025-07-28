d68257b20c993d22459b8a083ad4e5da1da37c5b
MalinkaKAW <154737367+MalinkaKAW@users.noreply.github.com>
2025-07-28 18:05:48 +0530

    added something

--- my-app/src/app/Services_home/Restaurent/restaurent.tsx
+++ my-app/src/app/Services_home/Restaurent/restaurent.tsx
@@ -262,7 +262,7 @@ const RestaurantRegistrationForm: React.FC = () => {
               <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
               <polyline points="22,4 12,14.01 9,11.01"></polyline>
             </svg>
-          </div>
+          <>
           <h1 className={styles.successTitle}>Restaurant Successfully Registered!</h1>
           <p className={styles.successMessage}>
             Congratulations! <strong>{formData.restaurantName}</strong> has been registered successfully.
